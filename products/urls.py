4316dab643fa48f1271254b99794196daa7edb92
Abdulhamid <abdulhamidkuchinskiy@gmail.com>
2026-07-17 00:41:37 +0500

    uxlash yaxshi lekin kop uxlash undanam yaxhsi

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ProductViewSet
+from .views import ProductViewSet,ProductUpdateAPIView
 
 urlpatterns = [
     path('', ProductViewSet.as_view({'get': 'list', 'post': 'create'})),
+    path('',ProductUpdateAPIView.as_view({'put': 'update', 'patch': 'partial_update'}))
 ]
--- products/views.py
+++ products/views.py
@@ -1,7 +1,11 @@
 from rest_framework import viewsets
-from .serializers import ProductSerializers
+from .serializers import ProductSerializers,UpdateAPIView
 from .models import Product
 
 class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializers
+
+class ProductUpdateAPIView(UpdateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializers
