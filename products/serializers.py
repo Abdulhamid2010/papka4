063c663e2439af1cb8a4df7c76abb62f9c273701
Abdulhamid <abdulhamidkuchinskiy@gmail.com>
2026-07-24 02:39:22 +0500

    prosta

--- products/serializers.py
+++ products/serializers.py
@@ -1,7 +1,23 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
-from .models import Product
 
-class ProductSerializers(serializers.ModelSerializer):
+User = get_user_model()
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
     class Meta:
-        model = Product
-        fields = ['id', 'name', 'category', 'price', 'description', 'stock', 'rating']
+        model = User
+        fields = ["id", "username", "email", "bio", "avatar", "password"]
+        read_only_fields = ["id"]
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data["username"],
+            email=validated_data["email"],
+            password=validated_data["password"],
+            bio=validated_data.get("bio", ""),
+            avatar=validated_data.get("avatar", None),
+        )
+        return user
